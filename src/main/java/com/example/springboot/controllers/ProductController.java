da408bd776cb0a6ae0021abdc970de5f0e8c9002
DiogoZampar <diogozampar@gmail.com>
2023-11-06 22:14:05 -0300

    PUT and DELETE product

--- src/main/java/com/example/springboot/controllers/ProductController.java
+++ src/main/java/com/example/springboot/controllers/ProductController.java
@@ -8,9 +8,11 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -60,6 +62,33 @@ public class ProductController {
             return ResponseEntity.ok(product);
         } 
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found!");
+
+    }
+
+
+    @PutMapping("/products/{id}")
+    public ResponseEntity<Object> updateProductByID(@PathVariable(value="id") UUID id, @RequestBody @Valid ProductRecordDTO productRecordDTO){
+        Optional<Product> product = productRepository.findById(id);     
+        if(product.isPresent()){
+            var productModel = product.get();
+            BeanUtils.copyProperties(productRecordDTO, productModel);
+            return ResponseEntity.ok(productRepository.save(productModel));
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found!");
+
+
+    }
+
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Object> deleteProductByID(@PathVariable(value="id") UUID id){
+        Optional<Product> product = productRepository.findById(id);
+        if(product.isPresent()){
+            productRepository.deleteById(id);
+            return ResponseEntity.ok("Product deleted!");
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found!");
 
     }
 
